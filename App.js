8859b2b3809166c824c0d693d787e643316cf1d6
anaBeatrizWalker <anabiawalker@gmail.com>
2023-04-12 08:36:50 -0300

    Melhorando aplicação

--- App.js
+++ App.js
@@ -6,32 +6,39 @@ import Vaga from './components/Vaga';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Vagas de Emprego de TI
-      </Text>
+      <Text style={styles.paragraph}>Vagas de Emprego de TI</Text>
       <ScrollView>
-        <Vaga 
+        <View>
+          <Vaga 
           titulo="Tech Lead Java" 
           salario="451260,63" 
           descricao="Liderar iniciativas para entrega de novos módulos e aplicações aos negócios/sistemas do grupo, definir padrões e jogar fut na praia." 
           contato="halftech@halftech.com"/>
-        <Vaga 
+        </View>
+
+        <View>
+          <Vaga 
           titulo="Desenvolver Sênios .NET" 
           salario="10520,20" 
           descricao="Traduzir storyboards e casos de uso em aplicativos funcionais, projetar, construir e manter códigos em C# eficientes e reutilizáveis, garantir o melhor desempenho, qualidade e capacidade de respostas dos aplicativos." 
           contato="dbcorpartion@db.com"/>
+        </View>
 
-        <Vaga 
+        <View>
+          <Vaga 
           titulo="Desenvolver Flutter" 
           salario="7412,00" 
           descricao="Conhecimentos sólidos em Flutter, padrões arquiteturais como MVVM. Necessário atuação anterior com Kotlin, Java, Engenharia de Software e Design Patterns. Experiência com XML e Custom Views." 
           contato="framework@framework.com"/>
-
-        <Vaga 
+        </View>
+        
+        <View>
+          <Vaga 
           titulo="Consultor Sênior SAP CCS FICA" 
           salario="13450,30" 
           descricao="Necessário curso superior completo em TI, conhecimentos sólidos funcionais e técnicos em SAP CCS FICA e experiência em Utilities para empresas de Energia." 
           contato="dxctech@dxctech.com"/>
+        </View>
       </ScrollView>
     </View>
   );
